9847543e1bcfd22d050b4fb3865bb7e78fa1ca90
Ashto <ashto13@hotmail.com>
2018-08-25 14:31:39 -0400

    Code refactoring, layout adjustments
    * Updated HTML tags to be more semantic
    * Quote author links to API's source page
    * Layout resizes properly across all devices
    * Added decorative quotation mark before quote text
    * Javascript documentation changed to JSDocs
    * ChangeColor() uses CSS's HSL and the Golden Ratio
      to randomly generate color instead of picking one
      out of a list
    * Fixed typo when trying to fail the Ajax call
    * InitializePage() called when page is ready

--- script.js
+++ script.js
@@ -1,66 +1,79 @@
-var quoteText = "";
-var authorText = "";    
+'use strict';
 
-/// <summary> Changes the background color that the page will use. </summary>
+var quoteText = "";   /** Holds the returned quote text */
+var authorText = "";  /** Holds the quote author's name */
+
+/**
+* Randomly generates a css color selection string, and applies it to the required elements.
+*/
 function ChangeColor() {
-  var color = ["FIREBRICK","DARKGREEN","SEAGREEN","DARKSLATEGRAY","DARKGOLDENROD","REBECCAPURPLE","DARKORANGE","SADDLEBROWN","DARKSLATEBLUE","BLACK"];
-  var i = Math.floor((Math.random() * 10));
 
-  $("body").css({background:color[i],color:color[i]});
-  $("#next").css({background:color[i]});
-  $("#twitter").css({backgroundColor:color[i]});
-  $("#tumblr").css({backgroundColor:color[i]});
+  var goldenRatio = 0.61803398875;
+  var rand = (Math.random() + goldenRatio) % 1;
+  rand = Math.floor(rand * 255);
+
+  var color = "hsl(" + rand + ", 70%, 28%)";
+
+  $("body").css({background:color,color:color});
+  $("#next").css({background:color});
+  $("#twitter, #tumblr").css({backgroundColor:color});
 }
 
-/// <summary> Ajax call that fetches quote data, parses the quote and author, and updates their respective containers. </summary>
-function LoadQuote() {
+/**
+* Ajax call that fetches quote data, parses the quote and author, and updates their respective containers.
+*/
+function FetchQuote() {
+  // Depending on where the file is hosted, cors-anywhere may be required
+  var cors = ""; // https://cors-anywhere.herokuapp.com/
 
   $("#quote, #author").slideUp();
 
   $.ajax({
-    url: "https://cors-anywhere.herokuapp.com/http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+    url: cors + "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",
     dataType: "json",
     cache : false,
+    timeout: 5000,
     success : function(quote) {
-      quoteText = CleanQuote(quote[0].content);
+      quoteText = $(quote[0].content).text();
       authorText = quote[0].title;
       $("#quote").html(quoteText).slideDown();
-      $("#author").html("&mdash; "+authorText).slideDown();
+      $("#author").html($('<a></a>')
+        .attr({'href': quote[0].link, 'target': '_blank'})
+        .html("&mdash; "+authorText)
+      ).slideDown();
     },
-    failure : function() {
+    error : function() {
       quoteText = "Something appears to have gone wrong. Please try again later.";
       $("#quote").html(quoteText).show();
     }
   });
 }
 
-/// <summary> A wrapper function to open up a new page. </summary>
-/// <param name="url" type="String"> The page to open. </param>
+/**
+* Helper function to open up an external page.
+* 
+* @param {String} The page to open.
+*/
 function LoadSite(url) {
   window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
 
-/// <summary> Cleans up quote text to avoid runtime errors. </summary>
-/// <param name="quote type="String"> The text to clean. </param>
-function CleanQuote(quote) {
-  return quote.replace(/(<([^>]+)>)/ig,"").trim();
-}
-
-/// <summary> Function that runs immediately on page load, in order to assure smooth animations
+/**
+* Calls ChangeColor and hides quote text at page load, to assure a clean starting point for visitors.
+*/
 function InitializePage() {
   $("#quote, #author").hide();
   ChangeColor();
 }
 
-InitializePage();
 
 $(document).ready(function(){
-  LoadQuote();
-
+  InitializePage();
+  FetchQuote();
 
   $("#next").on("click",function(){
     ChangeColor();
-    LoadQuote();
+    FetchQuote();
   });
 
 
